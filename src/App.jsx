2c777313d5d5d151cb5a64bf7144376af5168036
Fariya <fariya.webdev@gmail.com>
2025-10-18 01:42:30 +0600

    feat: banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -10,6 +11,8 @@ const App = () => {
         // className='min-h-[10000px] bg-indigo-200'
         >
             <Navbar/>
+            <Hero/>
+            <div className="h-dvh bg-black text-white">meaw</div>
         </main>
     );
 };
