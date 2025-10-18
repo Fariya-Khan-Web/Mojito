1443c577b1adb5860bb9d33b6b85520e9f383333
Fariya <fariya.webdev@gmail.com>
2025-10-19 03:29:27 +0600

    feat: menu navbar

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import { sliderLists } from '../../constants';
+import { useState } from 'react';
 
 const Menu = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const totalCocktails = sliderLists.length
+
+    const goToSlide = (idx) => {
+        const newIndex = (idx + totalCocktails) % totalCocktails
+        setCurrentIndex(newIndex)
+    }
+
+
     return (
-        <section id=''>
+        <section id='menu' aria-labelledby='menu-heading'>
+            <img src="/images/slider-left-leaf.png" alt="left-leaf" id='m-left-leaf' />
+            <img src="/images/slider-right-leaf.png" alt="right-leaf" id='m-right-leaf' />
+
+            <h2 id='menu-heading' className='sr-only'>
+                Cocktail Menu
+            </h2>
+
+            <nav className='cocktail-tabs' aria-label='Cocktail Navigation'>
+                {
+                    sliderLists.map((cocktail, idx) => {
+                        const isActive = idx === currentIndex;
+
+                        return (
+                            <button key={cocktail.id}
+                                className={`${isActive ?
+                                    'text-white border-white'
+                                    :
+                                    'text-white/50 border-white/50'}`}
+                                onClick={() => goToSlide(idx)}
+                            >
+                                {cocktail.name}
+                            </button>
+                        )
+                    })
+                }
+            </nav>
+
             
+
         </section>
     );
 };
